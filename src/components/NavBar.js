e00fe2e450039065507dafca9ac1a7dfaebe1c9e
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-09-23 20:42:40 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+     {links.map((link) => {
+          let navLinks = `#${link}`
+          return <a href={navLinks} key={Math.random()*1}>
+            {link}
+          </a>
+        })}
+   </nav>;
 }
 
 export default NavBar;
